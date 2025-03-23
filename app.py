38fc154bd3f24f71c0dae37aa2de365e4ce03f43
YeongsuKimm <yeongsuk505@gmail.com>
2025-03-22 19:04:35 -0700

    make template

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request, flash, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
